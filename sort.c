7963552f144ab94995b9045f75460702d5c9d72c
Andrew Brazhenko <andreybrazhenko@yandex.ru>
2020-02-12 20:53:59 +0300

    5 and 4 grades are now

--- sort.c
+++ sort.c
@@ -2,6 +2,7 @@
 #include "push_swap_lib/pushswap.h"
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 
 void		sort2(void)
 {
@@ -161,6 +162,8 @@ void	pop_from_a_and_b(size_t a_id, size_t b_id)
 	pop_up_from_b(b_id);
 }
 
+
+
 void	fill_one(void)
 {
 	size_t		min_steps_count;
@@ -174,8 +177,7 @@ void	fill_one(void)
 	i = 1;
 	while (i < b__()->size)
 	{
-		min_steps_count_tmp = (i > (b__()->size / 2) ? (b__()->size - i - 1)
-		: (i + 1)) + (1) + calc_min_steps_for_ok_place(b__()->stack[i]);
+		min_steps_count_tmp = (i > (b__()->size / 2) ? (b__()->size - i - 1) : (i + 1)) + (1) + calc_min_steps_for_ok_place(b__()->stack[i]);
 		if (min_steps_count_tmp < min_steps_count)
 			min_steps_count_num = i;
 		i++;
@@ -186,33 +188,158 @@ void	fill_one(void)
 	pa_();
 }
 
+
+
 //---------------------------fill_one2------------------------------------------
 
-size_t		calc_shortes_way_len(size_t a, size_t b)
-{
+#define A_UP_B_UP		0
+#define A_DOWN_B_DOWN	1
+#define A_UP_B_DOWN		2
+#define A_DOWN_B_UP		3
 
+
+static size_t	max(size_t x, size_t y)
+{
+	return (x > y ? x : y);
+}
+
+static void		process_put(size_t *arr)
+{
+	if (arr[5] == A_UP_B_UP)
+	{
+		while (arr[6] && arr[7])
+		{
+			rr_();
+			arr[6]--;
+			arr[7]--;
+		}
+		while (arr[6])
+		{
+			ra_();
+			arr[6]--;
+		}
+		while (arr[7])
+		{
+			rb_();
+			arr[7]--;
+		}
+	}
+	else if (arr[5] == A_DOWN_B_DOWN)
+	{
+		while (arr[6] && arr[7])
+		{
+			rrr_();
+			arr[6]--;
+			arr[7]--;
+		}
+		while (arr[6])
+		{
+			rra_();
+			arr[6]--;
+		}
+		while (arr[7])
+		{
+			rrb_();
+			arr[7]--;
+		}
+	}
+	else if (arr[5] == A_DOWN_B_UP)
+	{
+		while (arr[6])
+		{
+			rra_();
+			arr[6]--;
+		}
+		while (arr[7])
+		{
+			rb_();
+			arr[7]--;
+		}
+	}
+	else if (arr[5] == A_UP_B_DOWN)
+	{
+		while (arr[6])
+		{
+			ra_();
+			arr[6]--;
+		}
+		while (arr[7])
+		{
+			rrb_();
+			arr[7]--;
+		}
+	}
+	pa_();
 }
 
 /*
- * min[0] - iterator, min[1] - number of the element needs minimum operations,
- * to put from b to a.
- * min[2] - ok place in a, min[3] - current minimum steps, min[4] - global
- * minimum steps,
+ * arr[0] - steps to shift A elem up
+ * arr[1] - steps to shift A elem down
+ * arr[2] - steps to shift B elem up
+ * arr[3] - steps to shift B elem down
+ * arr[4] - current minimum steps
+ * arr[5] - current algorithm type
+ * arr[6] - position in A to put on
+ * arr[7] - position in B to take
  */
+
+void		calc_shortest_way_len(size_t a, size_t b, size_t *arr)
+{
+	arr[0] = a__()->size - a - 1;
+	arr[1] = a + 1;
+	arr[2] = b__()->size - b - 1;
+	arr[3] = b + 1;
+	// a вверх, b вниз
+	if (arr[0] + arr[3] < arr[4])
+	{
+		arr[4] = arr[0] + arr[3];
+		arr[5] = A_UP_B_DOWN;
+		arr[6] = arr[0];
+		arr[7] = arr[3];
+	}
+	// b вверх, a вниз
+	else if (arr[2] + arr[1] < arr[4])
+	{
+		arr[4] = arr[2] + arr[1];
+		arr[5] = A_DOWN_B_UP;
+		arr[6] = arr[1];
+		arr[7] = arr[2];
+	}
+	// обе вверх
+	else if (max(arr[0], arr[2]) < arr[4])
+	{
+		arr[4] = max(arr[0], arr[2]);
+		arr[5] = A_UP_B_UP;
+		arr[6] = arr[0];
+		arr[7] = arr[2];
+	}
+	//обе вниз
+	else if (max(arr[1], arr[3]) < arr[4])
+	{
+		arr[4] = max(arr[1], arr[3]);
+		arr[5] = A_DOWN_B_DOWN;
+		arr[6] = arr[1];
+		arr[7] = arr[3];
+	}
+
+}
+
 
 void		fill_one2(void)
 {
-	size_t		min[4];
-	size_t		a;
+	size_t		i;
+	size_t		arr[10];
 
-	min[0] = 0;
-	min[1] = 0;
-	while (min[0] < b__()->size)
+	arr[4] = 0 - 1;
+	calc_shortest_way_len(find_the_ok_place_in_a(b__()->stack[0]), 0, arr);
+	i = 1;
+	while (i < b__()->size)
 	{
-		a = find_the_ok_place_in_a(b__()->stack[min[0]]);
-
-		min[0]++;
+		calc_shortest_way_len(find_the_ok_place_in_a(b__()->stack[i]), i, arr);
+		i++;
 	}
+	// printf("shortest way: %zu, %zu, vals:%d, %d, count %zu\n", arr[6], arr[7], a__()->stack[arr[6]], b__()->stack[arr[7]], arr[4]);
+	process_put(arr);
 }
 
 void		normalize()
@@ -229,7 +356,7 @@ void		sortelse(void)
 	sort3();
 	while (b__()->size != 0)
 	{
-		fill_one();
+		fill_one2();
 	}
 	normalize();
 }
@@ -245,5 +372,5 @@ void 	sort(void)
 		sort3();
 	else
 		sortelse();
-	//show();
+	// show();
 }
